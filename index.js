b0811b9951703e28c3773c09979b11714fe014fa
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-12 08:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function prependKitten(name){
  return name.concat(kittens)
 }
 function removeLastKitten(){
- return kittens.slice(0,kittens.lenngth-1)
+ return kittens.slice(0,kittens.length-1)
 }
 function removeFirstKitten(){
  return kittens.slice(1)
