251c97fd856edf4e3cbf57ad18bf274519363ad8
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-12 07:49:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   kittens.pop(name)
 }
-function destructivelyPrependKitten(name){
-  kittens.pop(name)
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name)
 }
